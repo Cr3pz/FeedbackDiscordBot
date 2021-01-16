9c2b2f26d110bc22730c777e1770c2241e29053b
girth <aleyah.marroquin@gmail.com>
2021-01-16 23:48:09 +0000

    yes but more

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import random
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.utils import get
+from sys import argv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -22,23 +25,51 @@ async def on_ready():
 #	await member.dm_channel.send(f"sup {member.name}, let's get you set up")
 #	await member.dm_channel.send(f"send your info in this format: ~submit [Songname] [description IN PERENTHESIS] [link to your song]")
 #	await ctx.send('check your dms ;)')
-@bot.command(name='feedback')
-async def feedback(ctx, arg1, arg2, arg3):
+
+channel = bot.get_channel(800005895026901002)
+
+@bot.command(name='submit')
+@commands.has_role('Arole')
+async def submit(ctx,a: int, *, content):
+	role=get(ctx.message.server.roles, name='Arole')
+	message=ctx.message
 	member=ctx.author
-	a=arg1
-	b=arg2
-	c=arg3
-	channel = bot.get_channel(800005895026901002)
-	await channel.send("artist:{member.name} \nsong:%s \ndescription:%s %s", a,b,c)
+	link=content
+	progress=a
+	await bot.remove_roles(member, role)
+	await message.delete()
+	if progress == 1:
+		prog="Work in Progress"
+	elif progress == 2:
+		prog="Rough Draft"
+	elif progress == 3:
+		prog="Finished Song"
+	else:
+		await channel.send("Please enter a valid number, it's 1-3")
+		sauce=1
+	if progress<=3 and progress>=1:
+		await channel.send("artist:{} \nThis song is a {} \n{}".format(member.name, prog, link))
+		sauce=1
 
-@client.event
-async def on_message(message):
-	key='~feedback'
-	if key in message.content:
+
+@bot.event
+async def on_command_error(ctx, error):
+	if isinstance(error, commands.errors.CheckFailure):
+		message=ctx.message
+		await channel.send('**you have to give feedback before you can submit** {} **>:c**'.format(ctx.message.author.mention))
 		await message.delete()
+@bot.event
+async def on_message(message):
+	if "http" in message and not "~submit" or "~feedback"
+		await message.delete
+		await channel.send('only links for submissions or feedback examples O.O')
 
-#@bot.command(name='submit')
-#async def submit(ctx, arg1, arg2):
-
+@bot.command(name='feedback')
+async def feedback(ctx, arg1, *, content):
+	await message.delete()
+	if content.length<100:
+		await channel.send('you can give better feedback then that, sweeten it up 100+ characters')
+	else
+		
 
 bot.run(TOKEN)
